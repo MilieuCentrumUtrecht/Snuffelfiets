41dadbcf19435e12ec03672a9b3241f59d6e6bee
Michiel Kleinnijenhuis <michiel.kl@gmail.com>
2023-10-11 14:17:52 +0200

    Added civity colourscale to hexbin.

--- snuffelfiets/plotting.py
+++ snuffelfiets/plotting.py
@@ -9,6 +9,7 @@
 import numpy as np
 import plotly.figure_factory as ff
 import plotly.express as px
+from plotly.colors import hex_to_rgb
 
 
 def hexbin_mapbox(df, hexagon_size=None, hexbin_args={}, layout_args={}):
@@ -89,4 +90,41 @@ def save_fig(fig, outputstem, fig_formats=['html', 'pdf']):
             fig.write_html(f"{outputstem}.{fig_format}")
         else:
             fig.write_image(f"{outputstem}.{fig_format}")
+
+
+
+def discrete_colorscale(bvals=[], colors=[]):
+    """Functie to create discrete colorscale.
+
+    bvals -  list of values bounding intervals/ranges of interest
+    colors - list of rgb or hex color codes for values in [bvals[k], bvals[k+1]], 0 <= k < len(bvals)-1
+    returns  a nonuniform   discrete colorscale
+    """
+
+    if not bvals:
+        bvals = [
+            0, 3, 6, 9,
+            12, 18, 24, 30,
+            38, 46, 60, 100, 100,
+            ]
+    if not colors:
+        colors_hex =[
+            '#0020c5', '#006df8', '#2dcdfb', '#c4ecfd',
+            '#fffed0', '#fffc4d', '#f4e645', '#ffb255',
+            '#ff9845', '#fe7626', '#ff0a17', '#dc0625',
+            ]
+        colors =[f"rgb{hex_to_rgb(c)}" for c in colors_hex]
+
+    if len(bvals) != len(colors) + 1:
+        raise ValueError(f"len boundaries {len(bvals)} should be equal to len colours + 1 = {len(colors)+1}")
+
+    bvals = sorted(bvals)
+    nvals = [(v-bvals[0]) / (bvals[-1] - bvals[0]) for v in bvals]  # normalized values
+
+    dcolorscale = []
+    for k in range(len(colors)):
+        dcolorscale.extend([[nvals[k], colors[k]], [nvals[k+1], colors[k]]])
+
+    return dcolorscale 
+
 
