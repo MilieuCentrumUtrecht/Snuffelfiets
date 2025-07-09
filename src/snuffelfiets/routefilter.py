491c4e5807c84d22065c4f737fc0f29f5b4373d8
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2025-07-09 16:57:38 +0200

    Blacked routefilter files

--- src/snuffelfiets/routefilter.py
+++ src/snuffelfiets/routefilter.py
@@ -7,20 +7,28 @@ from os.path import isfile, join
 from pathlib import Path
 from datetime import datetime
 
-latMeter = 0.0000089988659514815        # 1 meter expressed in latitude (works for area province Utrecht)
-lonMeter = 0.0000146436902975532        # 1 meter expressed in longitude (works for area province Utrecht)
-latMin = 0 #51.858631                   # smallest latitude province Utrecht
-lonMin = 0 #4.794457                    # smallest longitude province Utrecht
-main_directory_default = Path(Path('~').expanduser(), 'Documents', 'MCUdataclub', 'RouteFilter')
+latMeter = 0.0000089988659514815  # 1 meter expressed in latitude (works for area province Utrecht)
+lonMeter = 0.0000146436902975532  # 1 meter expressed in longitude (works for area province Utrecht)
+latMin = 0  # 51.858631                   # smallest latitude province Utrecht
+lonMin = 0  # 4.794457                    # smallest longitude province Utrecht
+main_directory_default = Path(
+    Path("~").expanduser(), "Documents", "MCUdataclub", "RouteFilter"
+)
 
-def read_data(main_directory=main_directory_default, years=None, months=None, prefix="mcu_gegevens"):
+
+def read_data(
+    main_directory=main_directory_default,
+    years=None,
+    months=None,
+    prefix="mcu_gegevens",
+):
     if not years:
         years = [2024]
 
     if not months:
         months = [9]
 
-    data_directory = Path(main_directory, 'Input_Data')
+    data_directory = Path(main_directory, "Input_Data")
     if not data_directory.exists():
         Path.mkdir(data_directory, parents=True, exist_ok=True)
 
@@ -28,7 +36,7 @@ def read_data(main_directory=main_directory_default, years=None, months=None, pr
     df_list = []
     for year in years:
         for month in months:
-            filename = f'{prefix}_{year}-{month:02d}.csv'
+            filename = f"{prefix}_{year}-{month:02d}.csv"
             p = Path(data_directory, filename)
             try:
                 df = pd.read_csv(p)
@@ -38,24 +46,33 @@ def read_data(main_directory=main_directory_default, years=None, months=None, pr
     try:
         df = pd.concat(df_list, ignore_index=True)
     except:
-        print('\nThere is no dataframe df. \nNo data has been imported. \nExecution will crash now!')
+        print(
+            "\nThere is no dataframe df. \nNo data has been imported. \nExecution will crash now!"
+        )
         return df
 
     # add columns yLat and xLon to df
-    df['yLat'] = (df['latitude'] - latMin) / latMeter
-    df['xLon'] = (df['longitude'] - lonMin) / lonMeter
+    df["yLat"] = (df["latitude"] - latMin) / latMeter
+    df["xLon"] = (df["longitude"] - lonMin) / lonMeter
 
     # add column unique_rit_id
-    df['unique_rit_id'] = df['rit_id'] + df['year'] * 1000000 + df['month'] * 10000
+    df["unique_rit_id"] = df["rit_id"] + df["year"] * 1000000 + df["month"] * 10000
 
     # TODO: drop unnecessary columns?
     # df = df.drop(columns=['column_nameA', 'column_nameB'])
 
-    print(f'{df.shape[0]} measurements imported...')
+    print(f"{df.shape[0]} measurements imported...")
     return df
 
-def read_routes(main_directory=main_directory_default, years=None, months=None, prefix="mcu_gegevens", isExtend=True):
-    """ Function to filter Snuffelfiets measurements within a chosen distance of a route.
+
+def read_routes(
+    main_directory=main_directory_default,
+    years=None,
+    months=None,
+    prefix="mcu_gegevens",
+    isExtend=True,
+):
+    """Function to filter Snuffelfiets measurements within a chosen distance of a route.
 
     Function uses route csv file(s) to filter data from Snuffelfietsdata csv file(s).
     Function outputs filtered data per route.
@@ -72,11 +89,11 @@ def read_routes(main_directory=main_directory_default, years=None, months=None, 
         isExtend: Extend each route segment by distance.
 
     Returns:
-        dfO_list: List of dataframes with filtered measurements from Snuffelfiets data per route. 
+        dfO_list: List of dataframes with filtered measurements from Snuffelfiets data per route.
         dfR_list: List of route points per route.
 
     """
-    routes_directory = Path(main_directory, 'Input_Routes')
+    routes_directory = Path(main_directory, "Input_Routes")
     if not routes_directory.exists():
         Path.mkdir(routes_directory, parents=True, exist_ok=True)
 
@@ -87,42 +104,51 @@ def read_routes(main_directory=main_directory_default, years=None, months=None, 
     if len(routes) == 0:
         raise Exception("No routes given! Abort")
 
-    dfR_list = []                           # List for dfR (routes)
+    dfR_list = []  # List for dfR (routes)
     for filename in routes:
 
         p = Path(routes_directory, filename)
 
         try:
-            #TODO only import csv with correct columns and not empty
+            # TODO only import csv with correct columns and not empty
             dfR = pd.read_csv(p)
             print(f'[{filename}] {dfR["latitude"].shape[0]} route points read')
-            dfR.drop_duplicates(subset=['latitude', 'longitude'], inplace=True)
-            print(f'[{filename}] {dfR.shape[0]} unique route points remaining\n')
+            dfR.drop_duplicates(subset=["latitude", "longitude"], inplace=True)
+            print(f"[{filename}] {dfR.shape[0]} unique route points remaining\n")
             dfR.reset_index(inplace=True)
             dfR_list.append(dfR)
 
         except:
-            print(f'{filename} not valid for processing.\nMissing latitude and/or longitude columns (probably).\n')
+            print(
+                f"{filename} not valid for processing.\nMissing latitude and/or longitude columns (probably).\n"
+            )
             routes.remove(filename)
             continue
     return dfR_list, routes
 
-def filter_routes(dfR_list, routes, df, main_directory=main_directory_default, distance=10, isExtend=True):
-    output_directory = Path(main_directory, 'Output')
+
+def filter_routes(
+    dfR_list,
+    routes,
+    df,
+    main_directory=main_directory_default,
+    distance=10,
+    isExtend=True,
+):
+    output_directory = Path(main_directory, "Output")
     if not output_directory.exists():
         Path.mkdir(output_directory, parents=True, exist_ok=True)
-
 
     # Parameters
     timestamp = str(int(datetime.timestamp(datetime.now())))
 
     # Lists for dataframes
-    df_list = []                            # List for csv imports
-    dfS_list = []                           # List for dfS (measurements per segment)
-    dfO_list = []                           # List for dfO (output dataframes per route)
+    df_list = []  # List for csv imports
+    dfS_list = []  # List for dfS (measurements per segment)
+    dfO_list = []  # List for dfO (output dataframes per route)
 
     # Ignore expected error
-    warnings.filterwarnings("ignore", message='invalid value encountered in arc')
+    warnings.filterwarnings("ignore", message="invalid value encountered in arc")
 
     # use route points to filter df to max/min lat/lon
     maxLat = 0
@@ -131,112 +157,129 @@ def filter_routes(dfR_list, routes, df, main_directory=main_directory_default, d
     minLon = 999
 
     for dfR in dfR_list:
-        maxLatRoute = max(dfR['latitude'])
-        minLatRoute = min(dfR['latitude'])
-        maxLonRoute = max(dfR['longitude'])
-        minLonRoute = min(dfR['longitude'])
+        maxLatRoute = max(dfR["latitude"])
+        minLatRoute = min(dfR["latitude"])
+        maxLonRoute = max(dfR["longitude"])
+        minLonRoute = min(dfR["longitude"])
         maxLat = max(maxLatRoute, maxLat) + (distance * latMeter)
         minLat = min(minLatRoute, minLat) - (distance * latMeter)
         maxLon = max(maxLonRoute, maxLon) + (distance * lonMeter)
         minLon = min(minLonRoute, minLon) - (distance * lonMeter)
 
-    df = df[df['latitude'].between(minLat, maxLat )]
-    df = df[df['longitude'].between(minLon, maxLon)]
+    df = df[df["latitude"].between(minLat, maxLat)]
+    df = df[df["longitude"].between(minLon, maxLon)]
 
-    print(f'{df.shape[0]} filtered measurements remaining\n')
-    print(f'writing output to {output_directory}\n')
+    print(f"{df.shape[0]} filtered measurements remaining\n")
+    print(f"writing output to {output_directory}\n")
 
     # LOOP THROUGH ROUTES
     for idx, dfR in enumerate(dfR_list):
 
         # Reset dfS_list and routeSegment for this route
         dfS_list = []
-        df['routeSegment'] = 0
+        df["routeSegment"] = 0
 
         # Add columns to dfR and transform dfR into segments per row
-        dfR['longitude2'] = dfR['longitude'].shift(-1)
-        dfR['latitude2'] = dfR['latitude'].shift(-1)
+        dfR["longitude2"] = dfR["longitude"].shift(-1)
+        dfR["latitude2"] = dfR["latitude"].shift(-1)
         dfR.drop(dfR.tail(1).index, inplace=True)
-        dfR['xB'] = (dfR['longitude'] - lonMin) / lonMeter
-        dfR['yB'] = (dfR['latitude'] - latMin) / latMeter
-        dfR['xE'] = (dfR['longitude2'] - lonMin) / lonMeter
-        dfR['yE'] = (dfR['latitude2'] - latMin) / latMeter
-        print(f'created {dfR.shape[0]} segments from {routes[idx]}...')
+        dfR["xB"] = (dfR["longitude"] - lonMin) / lonMeter
+        dfR["yB"] = (dfR["latitude"] - latMin) / latMeter
+        dfR["xE"] = (dfR["longitude2"] - lonMin) / lonMeter
+        dfR["yE"] = (dfR["latitude2"] - latMin) / latMeter
+        print(f"created {dfR.shape[0]} segments from {routes[idx]}...")
 
         # Calculate segment length
-        dfR['BE'] = np.sqrt( 
-            np.square(dfR['xE'] - dfR['xB']) 
-            + np.square(dfR['yE'] - dfR['yB']) )
-        dfR['distBegin'] = dfR['BE'].cumsum().shift(1).fillna(0)
+        dfR["BE"] = np.sqrt(
+            np.square(dfR["xE"] - dfR["xB"]) + np.square(dfR["yE"] - dfR["yB"])
+        )
+        dfR["distBegin"] = dfR["BE"].cumsum().shift(1).fillna(0)
 
         # Extend segment from end point E with set distance
         # TODO: don't extend last segment somehow
-        if isExtend: 
-            dfR['xE'] = (dfR['xE'] - dfR['xB']) * (distance / dfR['BE']) + dfR['xE'] 
-            dfR['yE'] = (dfR['yE'] - dfR['yB']) * (distance / dfR['BE']) + dfR['yE'] 
-            dfR['BE'] = np.sqrt( 
-                np.square(dfR['xE'] - dfR['xB']) 
-                + np.square(dfR['yE'] - dfR['yB']) )
+        if isExtend:
+            dfR["xE"] = (dfR["xE"] - dfR["xB"]) * (distance / dfR["BE"]) + dfR["xE"]
+            dfR["yE"] = (dfR["yE"] - dfR["yB"]) * (distance / dfR["BE"]) + dfR["yE"]
+            dfR["BE"] = np.sqrt(
+                np.square(dfR["xE"] - dfR["xB"]) + np.square(dfR["yE"] - dfR["yB"])
+            )
 
         # LOOP THROUGH SEGMENTS OF ROUTE
         for index, segment in dfR.iterrows():
 
             # Calculate sides MB, ME using Pythagorean theorem
-            df['MB'] = np.sqrt( 
-                np.square(df['xLon'] - segment['xB']) 
-                + np.square(df['yLat'] - segment['yB']) ) 
-            df['ME'] = np.sqrt( 
-                np.square(df['xLon'] - segment['xE']) 
-                + np.square(df['yLat'] - segment['yE']) ) 
+            df["MB"] = np.sqrt(
+                np.square(df["xLon"] - segment["xB"])
+                + np.square(df["yLat"] - segment["yB"])
+            )
+            df["ME"] = np.sqrt(
+                np.square(df["xLon"] - segment["xE"])
+                + np.square(df["yLat"] - segment["yE"])
+            )
 
             # Calculate angles aBE, aMB, aME using Law of cosines (aBE is currently redundant)
-            df['aMB'] = np.acos( 
-                ( np.square(df['ME']) + np.square(segment['BE']) - np.square(df['MB']) ) 
-                / (2 * segment['BE'] * df['ME']) ) 
-            df['aME'] = np.acos( 
-                ( np.square(df['MB']) + np.square(segment['BE']) - np.square(df['ME']) ) 
-                / (2 * segment['BE'] * df['MB']) ) 
-            #df['aBE'] = np.acos( 
-                #( np.square(df['MB']) + np.square(df['ME']) - np.square(segment['BE']) ) 
-                #/ (2 * df['MB'] * df['ME']) ) 
+            df["aMB"] = np.acos(
+                (np.square(df["ME"]) + np.square(segment["BE"]) - np.square(df["MB"]))
+                / (2 * segment["BE"] * df["ME"])
+            )
+            df["aME"] = np.acos(
+                (np.square(df["MB"]) + np.square(segment["BE"]) - np.square(df["ME"]))
+                / (2 * segment["BE"] * df["MB"])
+            )
+            # df['aBE'] = np.acos(
+            # ( np.square(df['MB']) + np.square(df['ME']) - np.square(segment['BE']) )
+            # / (2 * df['MB'] * df['ME']) )
 
             # Calculate distances
-            df['M-BE'] = np.sin(df['aME']) * df['MB']                       # distance from measurement perpendicular to route segment line BE
-            df['distSegment'] = np.cos(df['aME']) * df['MB']                # distance measurement along segment
-            df['distRoute'] = df['distSegment'] + segment['distBegin']      # distance measurement along total route
+            df["M-BE"] = (
+                np.sin(df["aME"]) * df["MB"]
+            )  # distance from measurement perpendicular to route segment line BE
+            df["distSegment"] = (
+                np.cos(df["aME"]) * df["MB"]
+            )  # distance measurement along segment
+            df["distRoute"] = (
+                df["distSegment"] + segment["distBegin"]
+            )  # distance measurement along total route
 
             # Filter and add routeSegment number
             df.loc[
-                (df['M-BE'].between(0, distance)) 
-                & (df['aMB'].between(0, np.pi/2)) 
-                & (df['aME'].between(0, np.pi/2)) 
-                & (df['routeSegment'] == 0)
-                , ['routeSegment']] = index + 1
+                (df["M-BE"].between(0, distance))
+                & (df["aMB"].between(0, np.pi / 2))
+                & (df["aME"].between(0, np.pi / 2))
+                & (df["routeSegment"] == 0),
+                ["routeSegment"],
+            ] = (
+                index + 1
+            )
 
             # Save filtered measurements
-            dfS = df[df['routeSegment'] == index + 1]
-            #dfS['test2'] = 0 #why does this give a copywarning?
-            dfS_list.append(dfS)                                        # Append dfS to dfS_List
+            dfS = df[df["routeSegment"] == index + 1]
+            # dfS['test2'] = 0 #why does this give a copywarning?
+            dfS_list.append(dfS)  # Append dfS to dfS_List
 
         # Concat all dfS of route into dfO in dfO_list
         dfO_list.append(pd.concat(dfS_list, ignore_index=True))
 
-        output_filtered(idx, dfO_list, dfR, timestamp, routes, output_directory=output_directory)
+        output_filtered(
+            idx, dfO_list, dfR, timestamp, routes, output_directory=output_directory
+        )
 
 
 def output_filtered(idx, dfO_list, dfR, timestamp, routes, output_directory=None):
     # export dfO
-    filename2 = timestamp + '-dfOutput-' + routes[idx]
+    filename2 = timestamp + "-dfOutput-" + routes[idx]
     p = Path(output_directory, filename2)
     dfO_list[idx].to_csv(p, index=False)
-    print(f'...exported {dfO_list[idx].shape[0]} filtered measurements to {filename2}\n')
+    print(
+        f"...exported {dfO_list[idx].shape[0]} filtered measurements to {filename2}\n"
+    )
 
     # export dfR for testing
-    filename2 = timestamp + '-dfRoute-' + routes[idx]
+    filename2 = timestamp + "-dfRoute-" + routes[idx]
     p = Path(output_directory, filename2)
     dfR.to_csv(p, index=False)
 
     # export df for testing
-    #filename2 = timestamp  + '-df-' + routes[idx]
-    #p = Path(output_directory, filename2)
-    #df.to_csv(p, index=False)
+    # filename2 = timestamp  + '-df-' + routes[idx]
+    # p = Path(output_directory, filename2)
+    # df.to_csv(p, index=False)
--- tests/test_routefilter.py
+++ tests/test_routefilter.py
@@ -3,6 +3,7 @@ import pytest
 
 from snuffelfiets import routefilter as rf
 
+
 def test_filter_routes():
     dfR_list, routes = rf.read_routes()
     df = rf.read_data()
@@ -10,7 +11,10 @@ def test_filter_routes():
     root: Path = rf.main_directory_default
     output_directory = root / "Output"
     assert output_directory.exists(), "Output folder not created"
-    assert len(list(output_directory.iterdir())) == 8, "Wrong amount of output files created!"
+    assert (
+        len(list(output_directory.iterdir())) == 8
+    ), "Wrong amount of output files created!"
+
 
 if __name__ == "__main__":
     test_filter_routes()
