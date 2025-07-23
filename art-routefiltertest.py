40fe84e342ef25df4aa834086fa34b7361d7740d
Art-Gieteling <artgieteling>
2025-07-23 16:52:36 +0200

    routefilter learning 2

--- art-routefiltertest.py
+++ art-routefiltertest.py
@@ -1,5 +1,5 @@
 from src.snuffelfiets import routefilter as rf
 
-df = rf.read_data()
+df = rf.read_data(years=2019, months="5,6,7,8,9,10,11,12")
 dfR_list, routes = rf.read_routes()
 rf.filter_routes(dfR_list, routes, df)
