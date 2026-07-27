a6684b3700f8d6a98a99ea25b29a87540e739446
Michiel Kleinnijenhuis <michiel.kl@gmail.com>
2026-07-27 11:26:24 +0200

    Changed date-range to include end-date.

--- src/snuffelfiets/inlezen.py
+++ src/snuffelfiets/inlezen.py
@@ -109,7 +109,7 @@ def call_api(
 
     sql_conditions_dates = [
         {"col": "recording_timestamp", "op": ">", "val": start_datum},
-        {"col": "recording_timestamp", "op": "<", "val": stop_datum},
+        {"col": "recording_timestamp", "op": "<=", "val": stop_datum},
     ]
     sql_conditions_all = sql_conditions + sql_conditions_dates
 
